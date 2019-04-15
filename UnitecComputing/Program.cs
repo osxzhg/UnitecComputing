16ad1b955c44d691c372ca03a31836c04b56aaa4
osxzhg <osxzhg@gmail.com>
2019-04-15 21:15:49 +1200

    student: display delete update

--- UnitecComputing/Program.cs
+++ UnitecComputing/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using UnitecComputing.Data;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace UnitecComputing
 {
@@ -14,6 +16,21 @@ namespace UnitecComputing
     {
         public static void Main(string[] args)
         {
+            var host = CreateWebHostBuilder(args).Build(); //Added in Week 1
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<SchoolContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
             CreateWebHostBuilder(args).Build().Run();
         }
 
