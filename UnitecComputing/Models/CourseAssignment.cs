82fa272751d943cbe1d8ba6259ba1fcd1e359c87
osxzhg <osxzhg@gmail.com>
2019-04-18 21:44:07 +1200

    week4

--- UnitecComputing/Models/CourseAssignment.cs
+++ UnitecComputing/Models/CourseAssignment.cs
@@ -7,18 +7,12 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace UnitecComputing.Models
 {
-    public enum Grade
+    public class CourseAssignment
     {
-        A, B, C, D, F
-    }
-    public class Enrollment
-    {
-        public int EnrollmentID { get; set; }
+        public int InstructorID { get; set; }
         public int CourseID { get; set; }
-        public int StudentID { get; set; }
-        [DisplayFormat(NullDisplayText = "No grade")]
-        public Grade? Grade { get; set; }
+        public Instructor Instructor { get; set; }
         public Course Course { get; set; }
-        public Student Student { get; set; }
+
     }
 }
--- UnitecComputing/Models/Student.cs
+++ UnitecComputing/Models/Student.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace UnitecComputing.Models
 {
     public class Student
     {
         public int ID { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "LastName")]
         public string LastName { get; set; }
+        [StringLength(50,ErrorMessage ="First name cannot be longer than 50 characters.")]
+        [Display(Name ="FirstName")]
         public string FirstMidName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}",ApplyFormatInEditMode = true)]
         public DateTime EnrollmentDate { get; set; }
         public ICollection<Enrollment> Enrollments { get; set; }
     }
